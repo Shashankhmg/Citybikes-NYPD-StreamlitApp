a693872bd8088ad7b076d3452beed17dc21f3241
Shashankhmg <shashankh.m.g.1999@gmail.com>
2025-02-21 15:26:59 +0100

    changes after testing

--- pages/overview.py
+++ pages/overview.py
@@ -1,8 +1,4 @@
 import streamlit as st
-st.set_page_config(
-    page_title="CitiBike Risk Analysis",
-    layout="wide")
-
 import matplotlib.pyplot as plt
 from PIL import Image
 import pandas as pd
